186946a0a83089f829e7fa88d92e5c8a483c3703
Mariana Brasil <nayushi@qq.com>
2021-09-04 21:01:54 -0300

    Added load image functionality from imgur API.

--- Imgur Gallery/Home/ViewController.swift
+++ Imgur Gallery/Home/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SDWebImage
+import AVFoundation
 
 struct CustomData {
     var image = UIImage()
@@ -14,16 +15,26 @@ struct CustomData {
 
 class ViewController: UIViewController {
     
-    
+    // MARK: - Properties
     private let apiClient = ImagesAPIClient()
-    //var data: [FirstData] = []
+    var images: [String] = []
     
-
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Gallery"
+        
+        loadingImages()
+        setupView()
+        setupConstraint()
+        loadData()
+    }
+    
+    // MARK: - Methods
     fileprivate var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.minimumLineSpacing = 1
         layout.minimumInteritemSpacing = 1
-        
         
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
         cv.translatesAutoresizingMaskIntoConstraints = false
@@ -33,27 +44,36 @@ class ViewController: UIViewController {
         return cv
     }()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.title = "Gallery"
+    func getThumbnailImage(forUrl url: URL) -> UIImage? {
+        let asset: AVAsset = AVAsset(url: url)
+        let imageGenerator = AVAssetImageGenerator(asset: asset)
         
-        setupView()
-        setupConstraint()
-        loadData()
-        
-    
-        
+        do {
+            let thumbnailImage = try imageGenerator.copyCGImage(at: CMTimeMake(value: 1, timescale: 80) , actualTime: nil)
+            return UIImage(cgImage: thumbnailImage)
+        } catch let error {
+            print(error)
+        }
+        return nil
     }
     
-   
-    
     func loadImage() {
-        
         let imageView = SDAnimatedImageView()
         let animatedImage = SDAnimatedImage(named: "image.gif")
         imageView.image = animatedImage
-        
         imageView.sd_setImage(with: URL(string: "http://www.domain.com/path/to/image.jpg"))
+    }
+    
+    func loadingImages() {
+        let alert = UIAlertController(title: nil, message: "Loading images", preferredStyle: .alert)
+
+        let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
+        loadingIndicator.hidesWhenStopped = true
+        loadingIndicator.style = UIActivityIndicatorView.Style.medium
+        loadingIndicator.startAnimating();
+
+        alert.view.addSubview(loadingIndicator)
+        present(alert, animated: true, completion: nil)
     }
     
     
@@ -74,52 +94,45 @@ class ViewController: UIViewController {
         apiClient.fetchImages { result in
             switch result {
             case .success(let path):
-                let data = path.data
-                print(data)
-                
-                self.collectionView.reloadData()
-                //print("AQUI", self.data)
+                self.images = path
+                DispatchQueue.main.async {
+                    self.collectionView.reloadData()
+                    self.dismiss(animated: false, completion: nil)
+                }
             case .failure(let error):
                 print("Erro:", error.localizedDescription)
             }
         }
     }
-    
-    
-    
 }
 
 
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 2
+        return self.images.count
     }
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCell
-        
-        cell.backgroundColor = .blue
-        
-       /* let imgData = data[indexPath.row]
-       let imageView = SDAnimatedImageView()
-        let animatedImage = SDAnimatedImage(named: imgData.link)
-       imageView.image = animatedImage
-        */
-        
+        let imgData = images[indexPath.row]
+        let imgURL = URL(string: imgData)
+        if let url = imgURL {
+            if imgData.contains(".mp4") {
+                let mp4Image = getThumbnailImage(forUrl: url)
+                cell.img.image = mp4Image
+            } else {
+                cell.img.sd_setImage(with: url, completed: nil)
+            }
+        }
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let screenWidth = UIScreen.main.bounds.width
         let scaleFactor = (screenWidth / 4) - 1
-        
         return CGSize(width: scaleFactor, height: scaleFactor)
     }
     
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print("aaaa")
-    }
-    
-    
-    
+  
 }
--- Imgur Gallery/Network/API.swift
+++ Imgur Gallery/Network/API.swift
@@ -1,3 +1,10 @@
+//
+//  API.swift
+//  Imgur Gallery
+//
+//  Created by Mariana Brasil on 03/09/21.
+//
+
 import UIKit
 
 public enum Result<Value> {
@@ -5,23 +12,21 @@ public enum Result<Value> {
     case failure(Error)
 }
 
-
 public class ImagesAPIClient {
     
     // MARK: - Properties
     private let endpointURL = URL(string: "https://api.imgur.com/3/gallery/hot/viral/0")!
-    
+    var linkArray: [String] = []
     private let session: URLSession
     
-    // MARK: - Life Cycle
+    // MARK: - Lifecycle
     public init(session: URLSession = URLSession(configuration: .default)) {
         self.session = session
     }
     
     class DictionaryDecoder {
         private let jsonDecoder = JSONDecoder()
-
-        /// Decodes given Decodable type from given array or dictionary
+        
         func decode<T>(_ type: T.Type, from json: Any) throws -> T where T: Decodable {
             let jsonData = try JSONSerialization.data(withJSONObject: json, options: [])
             return try jsonDecoder.decode(type, from: jsonData)
@@ -29,33 +34,27 @@ public class ImagesAPIClient {
     }
     
     // MARK: - Methods
-    public func fetchImages(completion: @escaping (_ result: Result<FirstData>) -> Void) {
+    public func fetchImages(completion: @escaping (_ result: Result<[String]>) -> Void) {
         
         var request = URLRequest(url: endpointURL)
-        
         request.setValue("Bearer \(AppKeys.sharedInfo.accessToken)", forHTTPHeaderField: "Authorization")
-        
         let task = session.dataTask(with: request) { data, response, error in
             
-            if let httpResponse = response as? HTTPURLResponse {
-                
-                
-                if var data = data, let stringResponse = String(data: data, encoding: .utf8) {
-           
-                   
+            if (response as? HTTPURLResponse) != nil {
+                if let data = data, let _ = String(data: data, encoding: .utf8) {
                     do {
-                  
                         let decodedSentences = try JSONDecoder().decode(FirstData.self, from: data)
-     
-                        print(decodedSentences)
                         
-                       
-          
-                        DispatchQueue.main.async {
-                            completion(.success(decodedSentences))
+                        for element in decodedSentences.data {
+                            if let unwrappedImages = element.images {
+                                for image in unwrappedImages {
+                                    self.linkArray.append(image.link)
+                                }
+                            }
                         }
-                 
-        
+                        DispatchQueue.main.async {
+                            completion(.success(self.linkArray))
+                        }
                     } catch {
                         DispatchQueue.main.async {
                             completion(.failure(error))
@@ -69,15 +68,5 @@ public class ImagesAPIClient {
             }
         }
         task.resume()
-    }
-    
-    
-    
-}
-
-extension String {
-    func toJSON() -> Any? {
-        guard let data = self.data(using: .utf8, allowLossyConversion: false) else { return nil }
-        return try? JSONSerialization.jsonObject(with: data, options: .mutableContainers)
     }
 }
